404a8b536ee164ad34dfb285c6eb92fec6bf7ca6
seanpark <psckyky@hotmail.com>
2025-03-12 22:08:14 +0900

    [prototype] mqtt connections

--- experiments/test_mqtt.py
+++ experiments/test_mqtt.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 BROKER = "localhost"
 PORT = 1883
 CLIENT_ID = "client_1"
-STATUS_TOPIC = f"clients/{CLIENT_ID}/status"
+STATUS_TOPIC = f"clusters/{CLIENT_ID}/connections"
 
 def on_connect(client, userdata, flags, reason_code):
     print(f"[{CLIENT_ID}] Connected with Reason Code: {reason_code}")
@@ -12,13 +12,12 @@ def on_connect(client, userdata, flags, reason_code):
 def on_disconnect(client, userdata, reason_code):
     print(f"[{CLIENT_ID}] Disconnected with Reason Code: {reason_code}")
 
-client = mqtt.Client(client_id=CLIENT_ID)  # 여기 수정
+client = mqtt.Client(client_id=CLIENT_ID)
 
 # LWT 설정 (비정상 종료 시 자동으로 "offline" 메시지 전송)
 client.will_set(STATUS_TOPIC, "offline", qos=1, retain=True)
 
 client.on_connect = on_connect
-client.on_disconnect = on_disconnect
 
 client.connect(BROKER, PORT, 60)
 client.loop_forever()
